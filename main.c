5c6715db9b169a7f9bda3a490468bd99528ceae6
Jack Wherry <jack@snazz.xyz>
2023-03-09 18:32:39 -0600

    create global state object

--- main.c
+++ main.c
@@ -1,34 +1,39 @@
 #include "raycast.h"
 
+// Global state object
+struct {
+	SDL_Window *window;
+	bool quit;
+} state;
+
 int main(int argc, char* argv[]) {
 	// Print debugging messages about the user's computer
 	printSetupMessages();
 
 	assert(SDL_Init(SDL_INIT_VIDEO) == 0);
 
-	SDL_Window *window = SDL_CreateWindow(PROJECT_NAME, SDL_WINDOWPOS_CENTERED,
+	state.window = SDL_CreateWindow(PROJECT_NAME, SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		680, 480,
 		0);
-	assert(window);
+	assert(state.window);
 
-	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+	SDL_Surface *window_surface = SDL_GetWindowSurface(state.window);
 	assert(window_surface);
 
-	SDL_UpdateWindowSurface(window);
+	SDL_UpdateWindowSurface(state.window);
 
 	SDL_Event e;
-	bool quit = false;
-	while (!quit) {
+	while (!state.quit) {
 		while (SDL_PollEvent(&e)) {
 			if (e.type == SDL_QUIT) {
-				quit = true;
+				state.quit = true;
 			}
 			if (e.type == SDL_KEYDOWN) {
-				quit = true;
+				state.quit = true;
 			}
 			if (e.type == SDL_MOUSEBUTTONDOWN) {
-				quit = true;
+				state.quit = true;
 			}
 		}
 	}
